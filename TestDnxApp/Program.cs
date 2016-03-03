c21ed63194187dc045b0439adc607fa894bd1f99
Igor Manushin <igor.manushin@gmail.com>
2016-03-03 14:02:52 +0700

    support only one framework, add exceptions handling

--- TestDnxApp/Program.cs
+++ TestDnxApp/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,24 @@ namespace TestDnxApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            Trace.Listeners.Add(new ConsoleTraceListener()); 
+            
+            try
+            {
+                Console.WriteLine("Hello World");
+                Console.WriteLine();
+                Console.Read();                            
+            }   
+            catch(Exception ex)
+            {
+                Trace.TraceError("Unhandled exception: {0}", ex);
+                
+                return -1;
+            }   
 
-            Console.WriteLine("Hello World");
-            Console.WriteLine();
-            Console.Read();
+            return 0;
         }
     }
 }
