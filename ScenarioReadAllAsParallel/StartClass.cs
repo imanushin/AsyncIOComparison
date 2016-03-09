644d1af8c4ea98a0448cbe20d311783d1096beb0
Igor Manushin <igor.manushin@optimarea.com>
2016-03-09 14:06:30 +0300

    support Read All with parallel invokation

---
+++ ScenarioReadAllAsParallel/StartClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using IntermediateData;
+
+namespace ScenarioSyncAsParallel
+{
+    internal static class StartClass
+    {
+        private static int Main(string[] args)
+        {
+            var arguments = Arguments.Parse(args);
+
+            PerformanceCheck.CheckPerformance(arguments, ProcessFiles);
+
+            return 0;
+        }
+
+        private static int ProcessFiles(ImmutableList<string> filesList)
+        {
+            return (int)filesList.AsParallel().Select(CheckFileLength).Average();
+        }
+
+        private static int CheckFileLength(string filePath)
+        {
+            return File.ReadAllBytes(filePath).Length;
+        }
+    }
+}
--- TestsHost/Program.cs
+++ TestsHost/Program.cs
@@ -64,7 +64,7 @@ namespace TestsHost
 
             var arguments = new Arguments(filesListFile, resultsFile);
 
-            var scenarios = ImmutableList.Create("ScenarioSyncAsParallel", "ScenarioAsync", "ScenarioAsync2",
+            var scenarios = ImmutableList.Create("ScenarioSyncAsParallel", "ScenarioReadAllAsParallel", "ScenarioAsync", "ScenarioAsync2",
                 "ScenarioNewThread");
 
             CheckScenario("ScenarioAsync", arguments); //warm system io caches
