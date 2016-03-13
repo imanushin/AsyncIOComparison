1066c2b52a89673962669f4e0bcaf313f1dd0e2d
Igor Manushin <igor.manushin@optimarea.com>
2016-03-13 16:45:46 +0300

    add markdowns output

---
+++ TestsHost/MultipleExecutionResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TestsHost
+{
+    internal sealed class MultipleExecutionResult
+    {
+        public MultipleExecutionResult(
+            TimeSpan executionTime,
+            float averageProcessorTime,
+            float averageMemoryUsage, 
+            bool wasFailed)
+        {
+            ExecutionTime = executionTime;
+            AverageProcessorTime = averageProcessorTime;
+            AverageMemoryUsage = averageMemoryUsage;
+            WasFailed = wasFailed;
+        }
+
+        public TimeSpan ExecutionTime { get; }
+
+        public float AverageProcessorTime { get; }
+
+        public float AverageMemoryUsage { get; }
+
+        public bool WasFailed { get; }
+    }
+}
--- TestsHost/Program.cs
+++ TestsHost/Program.cs
@@ -13,6 +13,8 @@ namespace TestsHost
 {
     internal static class Program
     {
+        private const int AttemptsCount = 5;
+
         public static int Main()
         {
             Trace.Listeners.Add(new ConsoleTraceListener());
@@ -36,23 +38,30 @@ namespace TestsHost
             var outDir = Environment.CurrentDirectory;
             var outFile = Path.Combine(outDir, "results_") + DateTime.UtcNow.Ticks + ".md";
 
-            using (var file = File.Open(outFile, FileMode.Create))
+            using (var resultsFile = File.Open(outFile, FileMode.Create))
             {
-
-                foreach (var sizeDegree in Enumerable.Range(0, 7))
+                using (var resultsStream = new StreamWriter(resultsFile))
                 {
-                    var size = (int)Math.Pow(10, sizeDegree);
 
-                    await CheckFilesAsync(size).ConfigureAwait(false);
+                    foreach (var sizeDegree in Enumerable.Range(0, 7))
+                    {
+                        var size = (int)Math.Pow(10, sizeDegree);
+
+                        await CheckFilesAsync(size, resultsStream).ConfigureAwait(false);
+                    }
+
+                    await resultsStream.FlushAsync().ConfigureAwait(false);
+                    await resultsFile.FlushAsync().ConfigureAwait(false);
+                    await Console.Out.WriteLineAsync("Tests done").ConfigureAwait(false);
                 }
-
-                await file.FlushAsync().ConfigureAwait(false);
-                await Console.Out.WriteLineAsync("Tests done").ConfigureAwait(false);
             }
         }
 
-        private static async Task CheckFilesAsync(int minFileSize)
+        private static async Task CheckFilesAsync(int minFileSize, StreamWriter resultsStream)
         {
+            await resultsStream.WriteLineAsync("| Time | CPU usage (%) | Memory usage (b) | Was failed |").ConfigureAwait(false);
+            await resultsStream.WriteLineAsync("| -------- | -------- | -------- | -------- |").ConfigureAwait(false);
+            
             var filesToTest = FilesLookup.FindFiles(10000, minFileSize);
 
             var minSize = filesToTest.Select(f => f.Length).Min();
@@ -66,13 +75,14 @@ namespace TestsHost
             var currentFolder = Path.GetDirectoryName(exeFileName);
             Validate.IsNotNull(currentFolder);
             var filesListFile = Path.Combine(currentFolder, "filesToTest.json");
+
             await
                 FileNames.SaveFileListAsync(filesListFile, filesToTest.Select(fi => fi.FullName).ToImmutableList())
                     .ConfigureAwait(false);
 
-            var resultsFile = Path.Combine(currentFolder, "results.json");
+            var testResultsFilePath = Path.Combine(currentFolder, "results.json");
 
-            var arguments = new Arguments(filesListFile, resultsFile);
+            var arguments = new Arguments(filesListFile, testResultsFilePath);
 
             var scenarios = ImmutableList.Create(
                 "ScenarioAsyncWithMaxParallelCount4",
@@ -92,26 +102,52 @@ namespace TestsHost
 
             foreach (var scenario in scenarios)
             {
-                var testResult = CheckScenario(scenario, arguments);
-                var result = testResult.Data;
-                var cpuData = GetMinAvgMax(testResult.ProcessorTime);
-                var memData = GetMinAvgMax(testResult.MemoryUsage);
+                var multipleRunResults = Enumerable.Repeat(true, AttemptsCount).Select(_ => CheckScenario(scenario, arguments)).ToImmutableList();
 
-                if (testResult.ExitResult != ExitResult.Ok)
-                {
-                    await
-                        Console.Out.WriteLineAsync($"{scenario} - failed with {testResult.ExitResult} error")
-                            .ConfigureAwait(false);
-                }
-                else
-                {
-                    await
-                        Console.Out.WriteLineAsync($"{scenario} - {result.ExecutionTime.TotalSeconds} secs; cpu - {cpuData}, memory - {memData}")
-                            .ConfigureAwait(false);
-                }
+                var aggregatedResult = AggregateResult(multipleRunResults);
+                var testResult = CheckScenario(scenario, arguments);
+
+                await resultsStream.WriteLineAsync($"| {aggregatedResult.ExecutionTime} | {aggregatedResult.AverageProcessorTime} | {aggregatedResult.AverageMemoryUsage} | {aggregatedResult.WasFailed} |").ConfigureAwait(false);
+
+                await WriteScenarioResultsAsync(testResult, scenario).ConfigureAwait(false);
             }
 
             await Console.Out.WriteLineAsync().ConfigureAwait(false);
+        }
+
+        private static MultipleExecutionResult AggregateResult(ImmutableList<TestResult> multipleRunResults)
+        {
+            if (multipleRunResults.Any(r => r.ExitResult != ExitResult.Ok))
+            {
+                return new MultipleExecutionResult(TimeSpan.Zero, 0, 0, true);
+            }
+
+            var averageTime = TimeSpan.FromMilliseconds(multipleRunResults.Average(r => r.Data.ExecutionTime.TotalMilliseconds));
+            var averageCpu = multipleRunResults.Average(r => r.ProcessorTime.Average());
+            var averageMem = multipleRunResults.Average(r => r.MemoryUsage.Average());
+
+            return new MultipleExecutionResult(averageTime, averageCpu, averageMem, false);
+        }
+
+        private static async Task WriteScenarioResultsAsync(TestResult testResult, string scenario)
+        {
+            var result = testResult.Data;
+            var cpuData = GetMinAvgMax(testResult.ProcessorTime);
+            var memData = GetMinAvgMax(testResult.MemoryUsage);
+
+            if (testResult.ExitResult != ExitResult.Ok)
+            {
+                await
+                    Console.Out.WriteLineAsync($"{scenario} - failed with {testResult.ExitResult} error")
+                        .ConfigureAwait(false);
+            }
+            else
+            {
+                await
+                    Console.Out.WriteLineAsync(
+                        $"{scenario} - {result.ExecutionTime.TotalSeconds} secs; cpu - {cpuData}, memory - {memData}")
+                        .ConfigureAwait(false);
+            }
         }
 
         private static string GetMinAvgMax(ImmutableList<float> data)
